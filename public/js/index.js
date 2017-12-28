03e783fba3399163d5d4142dc0de5739cdc90c91
Chris Peterson <cdpeters1@gmail.com>
2017-12-28 14:31:40 -0800

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,31 +29,38 @@ socket.on('newLocationMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+/////////////////////////////////// listener: onsubmit new message form
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
+/////////////////////////////////// listener: onclick send location button
 var locationButton = jQuery('#send-location');
-
 // jQuery('#send-location').on is the same as the below:
 locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
